0f702791adeb47967d2eaffd6834599563962bfd
Jinan <lakanj25@gmail.com>
2026-07-10 03:22:24 +0100

    jul 10 03:22

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
 use std::net::TcpListener;
+use config::Environment;
 use secrecy::ExposeSecret;
 use sqlx::{PgPool};
 use zero2prod::{configuration::get_configuration, startup::run, telemetry::{get_subscriber, init_subscriber}};
@@ -13,10 +14,10 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await
+    let connection = PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
     run(listener, connection)?.await
 }
+
