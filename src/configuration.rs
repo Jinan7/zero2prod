a2f8e8034977878dc0ed8e7aac89271f517839e7
Jinan <lakanj25@gmail.com>
2026-08-03 09:21:34 +0100

    aug 3 09:21

--- src/configuration.rs
+++ src/configuration.rs
@@ -5,19 +5,21 @@ use sqlx::postgres::{PgConnectOptions, PgSslMode};
 use sqlx::ConnectOptions;
 use crate::domain::SubscriberEmail;
 #[derive(serde::Deserialize)]
+#[derive(Clone)]
 pub struct Settings {
     pub database: DatabaseSettings,
     pub application: ApplicationSettings,
     pub email_client: EmailClientSettings,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Clone)]
 pub struct EmailClientSettings {
     pub base_url: String, 
     sender_email: String,
     pub authorization_token: Secret<String>,
     pub timeout_milliseconds: u64,
 }
+
 
 impl EmailClientSettings {
     pub fn sender(&self) -> Result<SubscriberEmail, String> {
@@ -28,7 +30,8 @@ impl EmailClientSettings {
         std::time::Duration::from_millis(self.timeout_milliseconds)
     }
 }
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Clone)]
+
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
@@ -39,7 +42,7 @@ pub struct DatabaseSettings {
     pub require_ssl: bool,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Clone)]
 pub struct ApplicationSettings {
     #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
